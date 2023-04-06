848caab6320677d5dcc198a892176d2ceb86940a
sunil-kumar-sahu <100436257+sunil-kumar-sahu@users.noreply.github.com>
2023-04-06 11:15:36 +0530

    Add files via upload

---
+++ OOPS_Python1.py
@@ -0,0 +1,10 @@
+#Class And Object
+class DemoClass:
+    a=10
+
+    def sumvalue(self):
+        print(20+30)
+
+demoobject=DemoClass()#object creation
+print(demoobject.a)#print the value of variable by using object
+demoobject.sumvalue()#call sumvalue() function by using object
